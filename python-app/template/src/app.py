ddfc582758942f8d61a6aad700c7699e35f4c791
Daniel <cr9vercloud@gmail.com>
2026-03-26 22:59:33 +0100

    Added python app software template

---
+++ python-app/template/src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import socket
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+        'message': datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+        'hostname': socket.gethostname(),
+        'message2': 'This is a simple Python application running in Kubernetes!',
+        'deployed_on': 'Kubernetes',
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
+
+#'/api/v1/details'
+#'/api/v1/healthz'
+
+
